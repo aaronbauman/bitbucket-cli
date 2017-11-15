da4c65d97c5f98ce0d3a86952cb31aebd393a0e3
Martynas Sudintas <martynas12@gmail.com>
2017-11-15 22:34:34 +0200

    introduced bitbucket client interface

---
+++ src/Client/BitbucketClientInterface.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Martiis\BitbucketCli\Client;
+
+interface BitbucketClientInterface
+{
+    /**
+     * @param string $role
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function getTeamList(string $role, array $options = []);
+
+    /**
+     * @param string $owner
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function getTeamProjectList(string $owner, array $options = []);
+
+    /**
+     * @param string $username
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function getRepositoryList(string $username, array $options = []);
+
+    /**
+     * @param string $username
+     * @param string $repoSlug
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function getPullRequestList(string $username, string $repoSlug, array $options = []);
+
+    /**
+     * @param string $username
+     * @param string $repoSlug
+     * @param int    $pullRequestId
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function mergePullRequest(string $username, string $repoSlug, int $pullRequestId, array $options = []);
+
+    /**
+     * @param string $username
+     * @param string $repoSlug
+     * @param int    $pullRequestId
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function approvePullRequest(string $username, string $repoSlug, int $pullRequestId, array $options = []);
+
+    /**
+     * @param string $username
+     * @param string $repoSlug
+     * @param int    $pullRequestId
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function declinePullRequest(string $username, string $repoSlug, int $pullRequestId, array $options = []);
+}
